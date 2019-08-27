0e3dda2e8ccf7b9b829ca32db933180442e791d4
Kayla <kaynuhe@gmail.com>
2019-08-26 20:36:23 -0400

    style: re-style nav menu

--- themes/quotesondev/footer.php
+++ themes/quotesondev/footer.php
@@ -10,10 +10,9 @@
 			</div><!-- #content -->
 
 			<footer id="colophon" class="site-footer" role="contentinfo">
-				<h1>Footer!</h1>
-				<nav id="site-navigation" class="main-navigation" role="navigation">
-					<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button>
-					<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'primary-menu' ) ); ?>
+				<nav>
+					<!-- <button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php echo esc_html( 'Primary Menu' ); ?></button> -->
+					<?php wp_nav_menu( array( 'theme_location' => 'footer', 'menu_id' => 'Footer Menu' ) ); ?>
 				</nav><!-- #site-navigation -->
 				<div class="site-info">
 					<p>Brought to you by <a href="<?php echo esc_url( 'https://redacademy.com/toronto/' ); ?>">Red Academy</a></p>
--- themes/quotesondev/page-archives.php
+++ themes/quotesondev/page-archives.php
@@ -9,37 +9,50 @@ get_header(); ?>
 
 	<div id="primary" class="content-area">
 		<main id="main" class="site-main" role="main">
+			<?php if( have_posts()) : ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<h2><?php the_title(); ?></h2>
+				<?php endwhile;
+			endif; ?>
 
-		<?php $args = array(
+
+			<?php $args = array(
                 'post_type' => 'post',
                 'numberposts' => -1,
             );
 			$archives_list = get_posts($args); ?>
-			<div>Quote Authors: 
-
-            <?php foreach($archives_list as $post) :
+		
+			<h3>Quote Authors</h3>
+			<div class="archive-list">
+				<?php foreach($archives_list as $post) :
 				setup_postdata($post); ?>
 				
 					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 
-			<?php endforeach; ?>
+				<?php endforeach; ?>
 			</div>
-
-			<div>
-				<?php wp_list_categories(); ?>
+		
+			<h3>Categories</h3>
+			<div class="archive-list">
+				<?php wp_list_categories(array(
+					'separator' => ' ',
+					'style' => ' ',
+					'title_li' => ''
+				)); ?>
 			</div>
-
-			<div>Tags:
-
-			<?php
+			
+			<h3>Tags</h3>
+			<div class="archive-list">
+				<?php
 				$tags = get_tags(array(
 					'hide_empty' => false
 				));
 				// var_dump($tags);
 				foreach($tags as $tag) : ?>
-				<a href="<?php echo get_tag_link($tag->term_id); ?>"><?php echo $tag->name; ?></a>  
+					<a href="<?php echo get_tag_link($tag->term_id); ?>"><?php echo $tag->name; ?></a>  
 				<?php endforeach; ?>
 			</div>
+
 		</main><!-- #main -->
 	</div><!-- #primary -->
 
