e11f7403e7998a77f1983b359ffad7f9233b8e45
Kayla <kaynuhe@gmail.com>
2019-08-27 12:00:02 -0400

    style: category pg layout and numbered pagination

--- themes/quotesondev/category.php
+++ themes/quotesondev/category.php
@@ -11,32 +11,35 @@ get_header(); ?>
 		<main id="main" class="site-main" role="main">
 
 			<h2>Category: <?php single_cat_title(); ?></h2>
+			<hr>
 
 	
 			<?php
 			if( have_posts() ) : 
 				while( have_posts() ) :   
-				the_post(); ?>         
-
-				<div class="archive-page">
+				the_post(); ?> 
+				
+				
+				<div class="archive-block">
 					<h3 class="quote-text"><?php the_content(); ?></h3>
 					<?php $customField = get_post_custom(); ?>
-					<p class="mobile-author">-<?php the_title()?></p>
+					<h3 class="archive-author">&mdash; <?php the_title()?></h3>
 					<!-- Check if post has source and source url -->
 					<?php if(isset($customField['_qod_quote_source']) && isset($customField['_qod_quote_source_url'])) : ?>
-						<p>, <a href="<?php echo $customField['_qod_quote_source_url'][0]; ?>"><?php echo  $customField['_qod_quote_source'][0]; ?></a></p>
+						<p class="quote-source">, <a href="<?php echo $customField['_qod_quote_source_url'][0]; ?>"><?php echo  $customField['_qod_quote_source'][0]; ?></a></p>
 
 					<?php elseif(isset($customField['_qod_quote_source'])) : ?>
-						<p><?php echo ', ' . $customField['_qod_quote_source'][0]; ?></p>
+						<p class="quote-source"><?php echo ', ' . $customField['_qod_quote_source'][0]; ?></p>
 					<?php endif; ?>
 				
 				</div>
+				<hr>
 			
 				<?php endwhile; ?>
 		
 			
 
-			<?php the_posts_navigation(); ?> 
+				<?php qod_numbered_pagination(); ?>
 
 			<?php else : ?>
 					<p>No posts found</p>
